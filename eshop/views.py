cc39f83fcfdbc647a8923eb2f2ac6e8e0b3fe4d2
geosefuy <geosef_viktor_uy@dlsu.edu.ph>
2020-08-11 22:57:01 +0800

    template organization

--- eshop/views.py
+++ eshop/views.py
@@ -4,24 +4,24 @@ from .models import *
 # Create your views here.
 def cart(req):
     context = {}
-    return render(req, 'eshop/cart.html', context)
+    return render(req, 'pages/cart.html', context)
 
 def checkout(req):
     context = {}
-    return render(req, 'eshop/checkout.html', context)
+    return render(req, 'pages/checkout.html', context)
 
 def product_details(req):
     context = {}
-    return render(req, 'eshop/product_details.html', context)
+    return render(req, 'pages/product_details.html', context)
 
 def homepage(req):
     context = {}
-    return render(req, 'eshop/homepage.html', context)
+    return render(req, 'pages/homepage.html', context)
 
 def login(req):
     context = {}
-    return render(req, 'eshop/login.html', context)
+    return render(req, 'pages/login.html', context)
 
 def register(req):
     context = {}
-    return render(req, 'eshop/register.html', context)
+    return render(req, 'pages/register.html', context)
