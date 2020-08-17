3a32d7fd3d1509c9c0860fb5ec7098bcc0eb5664
issertmg <issertmg@gmail.com>
2020-08-17 20:31:04 +0800

    added added product-list

--- eshop/urls.py
+++ eshop/urls.py
@@ -3,9 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', homepage, name = 'homepage'),
-    path('cart/', cart, name = 'cart'),
-    path('checkout/', checkout, name = 'checkout'),
-    path('product_details/', product_details, name = 'product_details'),
-    path('login/', login, name = 'login'),
-    path('register/', register, name = 'register'),
+    path('product_list/', product_list, name = 'product_list'),
 ]
--- eshop/views.py
+++ eshop/views.py
@@ -2,26 +2,11 @@ from django.shortcuts import render
 from .models import *
 
 # Create your views here.
-def cart(req):
-    context = {}
-    return render(req, 'pages/cart.html', context)
-
-def checkout(req):
-    context = {}
-    return render(req, 'pages/checkout.html', context)
-
-def product_details(req):
-    context = {}
-    return render(req, 'pages/product_details.html', context)
 
 def homepage(req):
     context = {}
     return render(req, 'pages/homepage.html', context)
 
-def login(req):
+def product_list(req):
     context = {}
-    return render(req, 'pages/login.html', context)
-
-def register(req):
-    context = {}
-    return render(req, 'pages/register.html', context)
+    return render(req, 'pages/product_list.html', context)
