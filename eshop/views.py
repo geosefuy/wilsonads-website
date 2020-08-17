6763f9f9da3ef89664901e17384899aead74b6e3
issertmg <issertmg@gmail.com>
2020-08-17 22:09:29 +0800

    update on product rendering

--- eshop/views.py
+++ eshop/views.py
@@ -4,7 +4,17 @@ from .models import *
 # Create your views here.
 
 def homepage(req):
-    context = {}
+    # TODO include filter for each group
+    # Limits query to 8 products for each group
+    group_A_row_one_products = Product.objects.all()[:4]
+    group_A_row_two_products = Product.objects.all()[4:4]
+    group_B_row_one_products = Product.objects.all()[:4]
+    group_B_row_two_products = Product.objects.all()[4:4]
+    context = { 'group_A_row_one_products': group_A_row_one_products,
+                'group_A_row_two_products': group_A_row_two_products,
+                'group_B_row_one_products': group_B_row_one_products,
+                'group_B_row_two_products': group_B_row_two_products,
+    }
     return render(req, 'pages/homepage.html', context)
 
 def product_list(req):
