460d5b88c2620282ef773c4a4904ce2a1fd82136
issertmg <issertmg@gmail.com>
2020-08-19 19:11:29 +0800

    added slug field for cat & subcat

--- eshop/models.py
+++ eshop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 
@@ -12,14 +13,24 @@ class Customer(models.Model):
         return self.name
 
 class Category(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(default='')
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
     
     def __str__(self):
         return self.name
 
 class SubCategory(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(default='')
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(SubCategory, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
