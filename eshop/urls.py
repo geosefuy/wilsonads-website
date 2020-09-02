85a345ea0f29adc1c976e47e92aef35fa5bb937e
Geosef Uy <geosef_viktor_uy@dlsu.edu.ph>
2020-09-02 17:27:27 +0800

    Payment information account page

--- eshop/urls.py
+++ eshop/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('get_product/<int:product_id>', getProduct, name="get_product"),
     path('account/<int:account_id>/', update_profile, name = 'update_profile'),
     path('account/<int:account_id>/create_and_update_address', create_and_update_address, name="create_and_update_address"),
+    path('account/<int:account_id>/create_and_update_credit', create_and_update_credit, name="create_and_update_credit"),
     path('account/<int:account_id>/orders', account_orders, name = 'account_orders'),
     path('account/<int:account_id>/orders/<int:order_id>', order_details, name = 'order_details'),
     path('logout/', logout_view, name='logout_view'),
